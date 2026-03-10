072dd34fc9c2ded440e73878b6f50ef09c68747c
Giliarde da Silva Rodrigues <giliarder1994@gmail.com>
2026-03-10 17:39:42 -0300

    desenvolvendo autenticação com JWT

--- src/routes/produtos.routes.js
+++ src/routes/produtos.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const produtosControler = require("../controllers/produtos.controller");
+const produtosController = require("../controllers/produtos.controller");
+const { autenticar } = require("../middlewares/auth.middleware");
 
-router.get("/produtos/:id", produtosControler.buscarProdutos);
-router.get("/produtos", produtosControler.filtrarProdutos);
-router.post("/produtos", produtosControler.adicionarProdutos);
-router.put("/produtos/:id", produtosControler.editarProdutos);
-router.delete("/produtos/:id", produtosControler.deletarProdutos);
+router.get("/produtos", autenticar, produtosController.filtrarProdutos);
+router.get("/produtos/:id", autenticar, produtosController.buscarProdutos);
+router.post("/produtos", autenticar, produtosController.adicionarProdutos);
+router.put("/produtos/:id", autenticar, produtosController.editarProdutos);
+router.delete("/produtos/:id", autenticar, produtosController.deletarProdutos);
 
 module.exports = router;
