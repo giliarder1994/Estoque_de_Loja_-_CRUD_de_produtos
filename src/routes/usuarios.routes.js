d2e27767486717689722af8efd45533f05c72b10
Giliarde da Silva Rodrigues <giliarder1994@gmail.com>
2026-05-12 11:05:59 -0300

    Rename route from '/cadastrar' to '/registrar'

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const usuariosController = require("../controllers/usuarios.controller");
 
-router.post("/cadastrar", usuariosController.cadastrar);
+
+router.post("/registrar", usuariosController.cadastrar);
 router.post("/login", usuariosController.login);
 
 module.exports = router;
