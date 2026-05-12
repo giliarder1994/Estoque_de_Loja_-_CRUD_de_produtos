a33198e7e9a82be1dba5dea0ff0560c1ae813c6b
Giliarde da Silva Rodrigues <giliarder1994@gmail.com>
2026-05-12 11:00:40 -0300

    Refactor routes and middleware usage in app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const app = express();
-const porta = 3000;
 const produtosRouter = require("./routes/produtos.routes");
 const usuariosRouter = require("./routes/usuarios.routes");
 const { erroMiddleware } = require("./middlewares/erro.middleware");
 
+app.use(express.json());
 
-app.use(express.json())
 
-app.use(produtosRouter)
-
-app.use(usuariosRouter);
+app.use('/produtos', produtosRouter);
+app.use('/auth', usuariosRouter);
 
 app.use(erroMiddleware);
-
-
 
 module.exports = app;
